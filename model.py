8690609b46ebdfda1a859cb32f936be9340ddc74
anonmemouse <11628035+anonmemouse@users.noreply.github.com>
2021-06-10 11:06:31 +0300

    Update model.py

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ import difflib
 import re
 
 THESE_ARE_YOURS_ = '-- Out of these topics, these are yours: '
-nltk.download('all'
+nltk.download('all')
 nlp = en_core_web_sm.load()
 punctuations = string.punctuation
 stop_words = ['אני',
